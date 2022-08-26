5bb5e4709d19f1bfcecec80b3513695c38fa7a5c
BobrikovAleksey <bobrikov.spb@ya.ru>
2022-08-26 20:27:04 +0300

    add component Footer #6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoverComponent } from './components/cover/cover.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoverComponent,
+    FooterComponent,
     ToolbarComponent
   ],
   imports: [
