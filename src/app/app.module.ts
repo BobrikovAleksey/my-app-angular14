2fc0b526f24020439f3a6e3e8c7efdbf8a9468aa
BobrikovAleksey <bobrikov.spb@ya.ru>
2022-08-26 15:20:06 +0300

    add component Cover #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoverComponent } from './components/cover/cover.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CoverComponent,
     ToolbarComponent
   ],
   imports: [
